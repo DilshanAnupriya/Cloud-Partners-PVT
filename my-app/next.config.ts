1cfa80ffddb2901868c2ae83130909f760739031
Dilshan <dilshananupriya@icloud.com>
2025-08-20 12:13:56 +0530

    fix

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,16 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
+        unoptimized: true,   // 🔑 disables Image Optimization
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                port: "",
+                pathname: "/**",
             },
         ],
     },
-
+    output: "export",
 };
 
 export default nextConfig;
